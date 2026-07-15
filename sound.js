c67c1d6f9f286b2d7e1196a583812e9ef28a9aa8
Rainyyy-rin <dyzrinx2@gmail.com>
2026-07-15 16:50:18 +0700

    Update audio file paths and add progress sound

--- sound.js
+++ sound.js
@@ -1,26 +1,69 @@
-const perfectSound = new Audio("perfect.mp3");
-const goodSound = new Audio("good.mp3");
-const missSound = new Audio("miss.mp3");
-const timeoutSound = new Audio("timeout.mp3");
+const perfectSound = new Audio("sound/perfect.mp3");
+const goodSound = new Audio("sound/good.mp3");
+const missSound = new Audio("sound/miss.mp3");
+const progressSound = new Audio("sound/progress.mp3");
 
+progressSound.loop = true;
 
+function playProgress(){
+    progressSound.currentTime = 0;
+    progressSound.play();
+}
+
+function stopProgress(){
+    progressSound.pause();
+    progressSound.currentTime = 0;
+}
 
 function playPerfect(){
+
+    progressSound.pause();
+
     perfectSound.currentTime = 0;
     perfectSound.play();
+
+    perfectSound.onended = ()=>{
+        if(playing){
+            progressSound.play();
+        }
+    };
+
 }
 
 function playGood(){
+
+    progressSound.pause();
+
     goodSound.currentTime = 0;
     goodSound.play();
+
+    goodSound.onended = function(){
+
+        if(playing){
+
+            progressSound.play();
+
+        }
+
+    };
+
 }
 
 function playMiss(){
+
+    progressSound.pause();
+
     missSound.currentTime = 0;
     missSound.play();
-}
 
-function playTimeout(){
-    timeoutSound.currentTime = 0;
-    timeoutSound.play();
+    missSound.onended = function(){
+
+        if(playing){
+
+            progressSound.play();
+
+        }
+
+    };
+
 }
